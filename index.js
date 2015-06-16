b6379deb6cee7708c34a4f00d4218fecb02a5908
Ice White <yashiro1899@gmail.com>
2015-06-16 19:22:20 +0800

    1434453740

--- index.js
+++ index.js
@@ -6,18 +6,23 @@ try {
     }, function(err, data) {
         if (err) process.exit(1);
 
-        var et = data.exif["ExposureTime"] || "N/A";
-        var ec = data.exif["ExposureCompensation"] || "N/A";
-        var fl = data.exif["FocalLength"] || "N/A";
+        var et = data.exif["ExposureTime"];
+        var ec = data.exif["ExposureCompensation"];
+        var fl = data.exif["FocalLength"];
         if (typeof et === "number") {
             if (et < 1) et = "1/" + Math.round(1 / et);
             et += " sec";
+        } else {
+            et = "N/A";
         }
         if (typeof ec === "number") {
             if (ec !== 0) ec = Math.round(3 * ec) + "/3";
             ec += " EV"
+        } else {
+            ec = "N/A";
         }
         if (typeof fl === "number") fl = fl + " mm";
+        else fl = "N/A";
 
         console.log("    品牌: %s", data.image["Make"] || "N/A");
         console.log("    型号: %s", data.image["Model"] || "N/A");
